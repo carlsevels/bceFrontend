603f610fa688c82323d9225698eeb28c14ef7946
Carlos Vélez <80561943+carlsevels@users.noreply.github.com>
2026-01-18 17:23:56 -0600

    Solución error 404 al refrescar

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,17 @@
+import 'package:bcefrontend/infrastructure/navigation/navigation.dart';
+import 'package:bcefrontend/infrastructure/navigation/routes.dart';
 import 'package:flutter/material.dart';
-
 import 'package:get/get.dart';
-
-import 'infrastructure/navigation/navigation.dart';
-import 'infrastructure/navigation/routes.dart';
+import 'package:flutter_web_plugins/flutter_web_plugins.dart'; // Importa este
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  
+  setUrlStrategy(null); 
+
   var initialRoute = await Routes.initialRoute;
   runApp(Main(initialRoute));
 }
-
 class Main extends StatelessWidget {
   final String initialRoute;
   Main(this.initialRoute);
@@ -17,6 +19,7 @@ class Main extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
+      debugShowCheckedModeBanner: false, 
       initialRoute: initialRoute,
       getPages: Nav.routes,
     );
