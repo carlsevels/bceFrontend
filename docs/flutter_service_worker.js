4ea27daf4c5fde1a3cfea8a4350dbeba3cc586da
Carlos Vélez <80561943+carlsevels@users.noreply.github.com>
2026-01-20 18:56:25 -0600

    Update flutter web build

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "5ecf7bfd47efa46259ce04932b367d32",
+const RESOURCES = {"flutter_bootstrap.js": "81c744a08db25bae86dd1e4fd6e14d54",
 "version.json": "026f89f3eafb050feda6216118f79b66",
 "index.html": "157cfd3597330dedc9ef90b1bf9b4434",
 "/": "157cfd3597330dedc9ef90b1bf9b4434",
-"main.dart.js": "e25bbd1aae0e870f0121407cb6c6fcdb",
+"main.dart.js": "57291c0ebf8af4f7907815043f7f3bf0",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
